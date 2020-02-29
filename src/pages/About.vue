d7c9d4b405255479746d2e897d614ff377e1b2d8
Nwoke David <dvdnwoke@gmail.com>
2020-02-29 13:09:00 +0100

    Added router links

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,3 +1,4 @@
 <template>
   <h4>About Page</h4>
+  <router-link to="/profile/foo">View Your Profile</router-link>
 </template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,3 +1,4 @@
 <template>
   <h1>My Home Page</h1>
+  <router-link to="/about">About</router-link>
 </template>
