77f27d76c5d0c3331d0c41b888e560eee3bc6792
Nwoke David <dvdnwoke@gmail.com>
2020-02-29 13:31:39 +0100

    Added component guard on Profile.vue

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,12 +1,22 @@
 <template>
   <p>Username: {{route.params.username}}</p>
+  <router-link to="/">Home</router-link>
 </template>
 
 <script>
 import { inject, defineComponent, computed } from 'vue'
+import { onBeforeRouteLeave } from 'vue-router'
 export default defineComponent({
   setup () {
     const route = inject('route')
+    onBeforeRouteLeave((to, from, next) => {
+      const wantToLeave = window.confirm('Are you sure you want to leave your profile?')
+      if (wantToLeave) {
+        next()
+      } else {
+        next(false)
+      }
+    })
     return {
       route
     }
